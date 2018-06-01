c2ef0d78128d5d78e5691300660b051b4c39080c
Joshua Chaitin-Pollak <josh@offthehill.org>
2018-06-01 18:57:27 -0400

    tweaked personal settings

--- modules/jpollak/init.zsh
+++ modules/jpollak/init.zsh
@@ -96,6 +96,3 @@ alias sf='fasd -sif'     # interactive file selection
 alias z='fasd_cd -d'     # cd, same functionality as j in autojump
 alias zz='fasd_cd -d -i' # cd with interactive selection
 
-source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-source /usr/local/share/zsh-history-substring-search/zsh-history-substring-search.zsh
-
